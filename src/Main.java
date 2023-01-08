190813599c348f657a172eafadf8cf1309596d5e
Durdali Atılgan <33693720+durdaliatilgan@users.noreply.github.com>
2023-01-08 23:22:06 +0300

    buffer path update

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
             ReadBuffer in = new ReadBuffer(); //proses çıktıları için buffer.
             //dosyadan processlerin okunması ve gerekli kuyruklara yerleştirilmesi.
-            ArrayList<Process> processes = in.getProcesses("C:\\Users\\durda\\IdeaProjects\\untitled\\src\\veriler.txt");
+            ArrayList<Process> processes = in.getProcesses("giriş.txt");
             //proseslerin yönetilmesi.
             ArrayList<ProcessProcessing> gantt = new Scheduling().getSchedular(processes);
             //sonuc yazdır.
